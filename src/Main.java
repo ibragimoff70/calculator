4b7113d34014ebbc57dba8540d318d8b6ab31df0
Ibrohim <ibrohim.xasanov209@gmail.com>
2023-11-20 20:46:35 +0500

    salom

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
-
     public static void main(String[] args) {
-        int[] array = {1, -2, 0, 3, -4, 0, 5};
-
-        int nol = 0;
-        int musbat = 0;
-        int manfiy = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                nol++;
-            } else if (array[i] > 0) {
-                musbat++;
-            } else {
-                manfiy++;
-            }
-        }
-            }
-        }
+        System.out.println("Hello World!");
+    }
+}
