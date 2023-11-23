f2bd1343bf5b2124a98c4f0d4d2215956d9509e8
Mukhammadaziz <rustamov977075073@gmail.com>
2023-11-23 16:57:45 +0500

    rustamov

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.print("Bitta son kiriting: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        System.out.print("Bitta son kiriting: ");
+
+        int b = scanner.nextInt();
+
+        System.out.println("Nmani tanlisz?\n1.'+'\n2.'-'\n3.'*'\n4.':'");
+
+        int c = scanner.nextInt();
+
+        switch (c){
+            case 1-> System.out.println(a+b);
+            case 2->System.out.println(a-b);
+            case 3->System.out.println(a*b);
+            case 4->System.out.println(a/b);
+            default ->{ System.out.println("Error");
+                main(args);
+            }
+        }
+        main(args);
     }
 }
