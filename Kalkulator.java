d2c17852bdb9252e68ec4aab05d7676e66579b5c
Odilbek Normamadov <example@gmail.com>
2023-11-23 16:47:32 +0500

    calculator1

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Kalkulator {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        while(true){
+            System.out.print("1-son kiriting:");
+            int a1=scanner.nextInt();
+
+            System.out.print("2-son kiriting:");
+            int a2=scanner.nextInt();
+
+            System.out.print("amalni kiriting:");
+            String s=scanner.next();
+
+            if(s.equals("+")){
+                System.out.println(a1+"+"+a2+"="+(a1+a2));
+            } else if (s.equals("-")) {
+                System.out.println(a1+"-"+a2+"="+(a1-a2));
+            } else if (s.equals("*")) {
+                System.out.println(a1+"*"+a2+"="+(a1*a2));
+            } else if (s.equals("/")) {
+                System.out.println(a1+"/"+a2+"="+(a1/a2));
+            }else {
+                System.out.println("hato son kiritildi");
+            }
+        }
+
+    }
 }
